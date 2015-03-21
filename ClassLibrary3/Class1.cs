606bc92ec746abbfb2527a0ea0a8c4fe54bf92bf
tugberkugurlu <tugberkugurlu@gmail.com>
2015-03-21 13:20:34 +0000

    init

---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary3
+{
+    public class Class1
+    {
+        [Fact]
+        public Task Foo()
+        {
+            Assert.True(true);
+            return Task.FromResult(0);
+        }
+    }
+}
